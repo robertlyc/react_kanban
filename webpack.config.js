fa4caa89aabfe9fe1e339e3d8254eb23fc4064bc
merlinlyc <merlinlyc@gmail.com>
2015-12-23 11:36:44 +0800

    refact the webpack.config.js to add multi target

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,22 @@
 var path = require('path');
 var webpack = require('webpack');
 var HtmlwebpackPlugin = require('html-webpack-plugin');
+var merge = require('webpack-merge');
 
+const TARGET = process.env.npm_lifecycle_event;
 const PATHS = {
   app: path.join(__dirname, 'app'),
   build: path.join(__dirname, 'build')
 };
 
-module.exports = {
+var common = {
   entry: PATHS.app,
-  output: {
-    path: PATHS.build,
-    filename: 'bundle.js'
-  },
+  //Given webpack-dev-server runs in-memory, we can drop output.
+  //We'll look into it again once we get to the build chapter.
+  // output: {
+  //   path: PATHS.build,
+  //   filename: 'bundle.js'
+  // },
   module: {
     loaders: [
       {
@@ -22,21 +26,30 @@ module.exports = {
       }
     ]
   },
-  devServer: {
-    historyApiFallback: true,
-    hot: true,
-    inline: true,
-    progress: true,
-    
-    stats: 'errors-only',
-    
-    host: process.env.HOST,
-    port: process.env.PORT
-  },
   plugins: [
-    new webpack.HotModuleReplacementPlugin(),
+    // new webpack.HotModuleReplacementPlugin(),
     new HtmlwebpackPlugin({
       titel: 'Kanban app'
     })
   ]
 };
+
+if (TARGET === 'start' || !TARGET) {
+  module.exports = merge(common, {
+    devtool: 'eval-source-map',
+    devServer: {
+      historyApiFallback: true,
+      hot: true,
+      inline: true,
+      progress: true,
+    
+      stats: 'errors-only',
+    
+      host: process.env.HOST,
+      port: process.env.PORT
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin()
+    ]
+  });
+}
