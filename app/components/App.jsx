8560430a7e11766f65fcd83543192deef52ee9af
merlinlyc <merlinlyc@gmail.com>
2015-12-23 23:36:45 +0800

    pushing notes to the app state

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -2,23 +2,30 @@ import uuid from 'node-uuid';
 import React from 'react';
 import Note from './Notes.jsx';
 
-const notes = [
-  {
-    id: uuid.v4(),
-    task: 'Learn Webpack'
-  },
-  {
-    id: uuid.v4(),
-    task: 'Learn React'
-  },
-  {
-    id: uuid.v4(),
-    task: 'Do laundry'
-  }
-];
-
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+  
+    this.state = {
+      notes: [
+        {
+          id: uuid.v4(),
+          task: 'Learn Webpack'
+        },
+        {
+          id: uuid.v4(),
+          task: 'Learn React'
+        },
+        {
+          id: uuid.v4(),
+          task: 'Do laundry'
+        }
+      ]
+    };
+  }
+  
   render() {
+    const notes = this.state.notes;
     return (
       <div>
         <Note items={notes} />
