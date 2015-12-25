05101ec30190c55fb069e82727a0595cac9b9f4f
merlinlyc <merlinlyc@gmail.com>
2015-12-25 11:15:13 +0800

    implement CRUD methods

--- app/store/NoteStore.js
+++ app/store/NoteStore.js
@@ -5,15 +5,36 @@ import NoteActions from '../actions/NoteActions';
 class NoteStore {
   constructor() {
     this.bindActions(NoteActions);
-  
+    
     this.notes = [];
   }
   
-  create(note) {}
+  create(note) {
+    cost notes = this.notes;
+    note.id = uuid.v4();
+    
+    this.setState({
+      notes: notes.concat(note)
+    });
+  }
   
-  update({id, task}) {}
+  update({id, task}) {
+    const notes = this.notes.map((note) =>{
+      if (note.id === id) {
+        note.task = task;
+      }
+      
+      return note;
+    });
+    
+    this.setState({notes});
+  }
   
-  delete(id) {}
+  delete(id) {
+    this.setState({
+      notes: this.notes.filter((note) => note.id !== id)
+    });
+  }
 }
 
 export default alt.createStore(NoteStore, 'NoteStore');
