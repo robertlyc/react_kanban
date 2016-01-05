bec53315135b0ce44f3bb8d0967e5c14234dbe30
merlinlyc <merlinlyc@gmail.com>
2016-01-05 19:22:14 +0800

    Indicate where to move

--- app/components/Note.jsx
+++ app/components/Note.jsx
@@ -7,6 +7,10 @@ const noteSource = {
     return {
       id: props.id
     };
+  },
+  
+  isDragging(props, monitor) {
+    return props.id === monitor.getItem().id;
   }
 };
 
@@ -22,8 +26,10 @@ const noteTarget = {
   }
 };
 
-@DragSource(ItemTypes.NOTE, noteSource, (connect) => ({
-  connectDragSource: connect.dragSource()
+@DragSource(ItemTypes.NOTE, noteSource, (connect, monitor) => ({
+  connectDragSource: connect.dragSource(),
+  
+  isDragging: monitor.isDragging()
 }))
 
 @DropTarget(ItemTypes.NOTE, noteTarget, (connect) => ({
@@ -32,10 +38,10 @@ const noteTarget = {
 
 export default class Note extends React.Component {
   render() {
-    const {connectDragSource, connectDropTarget, id, onMove, ...props} = this.props;
+    const {connectDragSource, connectDropTarget, isDragging, id, onMove, ...props} = this.props;
     
     return connectDragSource(connectDropTarget(
-      <li {...props}>{props.children}</li>
+      <li style={{opacity: isDragging ? 0 : 1}}{...props}>{props.children}</li>
     ));
   }
 }
