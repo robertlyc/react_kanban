d2e9a88189854d5dc447832912ccb4b3d6eb4d8e
merlinlyc <merlinlyc@gmail.com>
2016-01-05 17:02:22 +0800

    bug fix

--- app/components/Note.jsx
+++ app/components/Note.jsx
@@ -1,21 +1,25 @@
 import React from 'react';
 import {DragSource} from 'react-dnd';
-import ItemType from '../constants/itemTypes';
+import ItemTypes from '../constants/itemTypes';
 
 const noteSource = {
-  beginDrage(props) {
+  beginDrag(props) {
     console.log('begin dragging note', props);
     
     return {};
   }
 };
 
-@DragSource(ItemType.NOTE, noteSource, (connect) => ({
+@DragSource(ItemTypes.NOTE, noteSource, (connect) => ({
   connectDragSource: connect.dragSource()
 }))
 
 export default class Note extends React.Component {
   render() {
-    return <li {...this.props}>{this.props.children}</li>;
+    const {connectDragSource, id, noMove, ...props} = this.props;
+    
+    return connectDragSource(
+      <li {...props}>{props.children}</li>
+    );
   }
 }
