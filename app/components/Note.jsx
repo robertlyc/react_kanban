8097519bfb68b96fc5d3ff116f099b527cbf6dad
merlinlyc <merlinlyc@gmail.com>
2016-01-05 16:53:08 +0800

    set up note @dragSource

--- app/components/Note.jsx
+++ app/components/Note.jsx
@@ -1,4 +1,18 @@
 import React from 'react';
+import {DragSource} from 'react-dnd';
+import ItemType from '../constants/itemTypes';
+
+const noteSource = {
+  beginDrage(props) {
+    console.log('begin dragging note', props);
+    
+    return {};
+  }
+};
+
+@DragSource(ItemType.NOTE, noteSource, (connect) => ({
+  connectDragSource: connect.dragSource()
+}))
 
 export default class Note extends React.Component {
   render() {
