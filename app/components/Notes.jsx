68680bc341dd897efc3aa6c8d23e5dafe3db3ef7
merlinlyc <merlinlyc@gmail.com>
2016-01-05 17:41:20 +0800

    add onMove method

--- app/components/Notes.jsx
+++ app/components/Notes.jsx
@@ -6,7 +6,10 @@ export default ({notes, onValueClick, onEdit, onDelete}) => {
   return (
     <ul className="notes">{notes.map((note) => {
       return (
-        <Note className="note" id={note.id} key={note.id}>
+        <Note className="note" id={note.id} key={note.id} 
+        onMove={({sourceId, targetId}) => 
+          console.log('source', sourceId, 'target', targetId)
+        }>
           <Editable 
             editing={note.editing}
             value={note.task}
